95faf12ddbc4b6af82bcd8b452c67f211ddcaa72
levi.tolnai <levente.tolnai@stud.uni-obuda.hu>
2025-05-08 12:02:28 +0200

    Student, Subject models done

---
+++ D8M7Q2_Sztgui_FF/Models/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D8M7Q2_Sztgui_FF.Models
+{
+    public class Student : INotifyPropertyChanged
+    {
+        private string id;
+        private string firstName;
+        private string lastName;
+        private ObservableCollection<Subject> subjects;
+
+        public string Id
+        {
+            get => id;
+            set { id = value; OnPropertyChanged(nameof(Id)); }
+        }
+        public string FirstName
+        {
+            get => firstName;
+            set { firstName = value; OnPropertyChanged(nameof(FirstName)); }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set { lastName = value; OnPropertyChanged(nameof(LastName)); }
+        }
+        public ObservableCollection<Subject> Subjects
+        {
+            get => subjects;
+            set { subjects = value; OnPropertyChanged(nameof(Subjects)); }
+        }
+        public Student(string firstName, string lastName)
+        {
+            Id = Guid.NewGuid().ToString();
+            FirstName = firstName;
+            LastName = lastName;
+            Subjects = new ObservableCollection<Subject>();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        public string FullName
+        {
+            get => $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ D8M7Q2_Sztgui_FF/Models/Subject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D8M7Q2_Sztgui_FF.Models
+{
+    public class Subject: INotifyPropertyChanged
+    {
+        private string name;
+        private int grade;
+
+        public string Name
+        {
+            get => name;
+            set { name = value; OnPropertyChanged(nameof(Name)); }
+        }
+        public int Grade
+        {
+            get => grade;
+            set { grade = value; OnPropertyChanged(nameof(Grade)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    }
+}
