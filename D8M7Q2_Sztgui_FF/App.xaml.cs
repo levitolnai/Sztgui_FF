146105d16d93069053bd23cd06ff831c1f17bc65
levi.tolnai <levente.tolnai@stud.uni-obuda.hu>
2025-05-08 10:55:13 +0200

    Add project files.

---
+++ D8M7Q2_Sztgui_FF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace D8M7Q2_Sztgui_FF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
